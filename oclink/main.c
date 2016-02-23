bc4040d86a3461793ae0334ab74033767eb72b92
Morgan Lieberthal <morgan@jmorgan.org>
2016-02-23 11:43:13 -0700

    Initial Commit

---
+++ oclink/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  oclink
+//
+//  Created by Morgan Lieberthal on 2/23/16.
+//  Copyright © 2016 J. Morgan Lieberthal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
