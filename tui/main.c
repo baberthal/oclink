925d357529fa61bd973c15d1d5136830c636473b
Morgan Lieberthal <morgan@jmorgan.org>
2016-02-24 16:24:45 -0700

    Add initial config for TUI build

---
+++ tui/main.c
@@ -0,0 +1,30 @@
+/* OpenCorsairLink TUI */
+/* Copyright © 2016 J. Morgan Lieberthal */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "tui_config.h"
+
+int main(int argc, char *argv[])
+{
+    initscr();
+    printw("Hello World !!!");
+    refresh();
+    getch();
+    endwin();
+
+    return 0;
+}
+
+/* vim: set ts=8 sw=4 tw=0 ft=c et :*/
