74e210addaaebbc669b051e94e745bede320b9f8
Morgan Lieberthal <morgan@jmorgan.org>
2016-03-24 13:54:13 -0600

    Clean Up Test Runner and tests

--- test/ocl_tests.c
+++ test/ocl_tests.c
@@ -3,42 +3,34 @@
 
 #include "clink.h"
 
-static OCL_Link *link = NULL;
+static OCL_Link *oclink = NULL;
 
 static void basic_setup(void)
 {
-    link = ocl_link_alloc();
+    oclink = ocl_link_alloc();
 }
 
 static void setup(void)
 {
     basic_setup();
-    ocl_link_init(link);
+    ocl_link_init(oclink);
 }
 
 static void teardown(void)
 {
-    if (link == NULL) return;
+    if (oclink == NULL) return;
 
-    if (link->handle != NULL) {
-        ocl_link_close(link);
+    if (oclink->handle != NULL) {
+        ocl_link_close(oclink);
     }
 
-    ocl_link_free(link);
+    ocl_link_free(oclink);
 }
-
-START_TEST(test_link_init)
-{
-    int rc = -1;
-    rc = ocl_link_init(link);
-    ck_assert_msg(rc == 0, "Failed to initialize link.");
-}
-END_TEST
 
 START_TEST(test_link_get_device_id)
 {
     int rc = 0;
-    rc = ocl_link_get_device_id(link);
+    rc = ocl_link_get_device_id(oclink);
     fprintf(stderr, "Link DeviceID is %x\n", rc);
     ck_assert_int_ne(rc, 0);
 }
@@ -47,7 +39,7 @@ END_TEST
 START_TEST(test_get_fw_version)
 {
     int rc = 0;
-    rc = ocl_link_get_fw_version(link);
+    rc = ocl_link_get_fw_version(oclink);
     fprintf(stderr, "Device FW Version is %X\n", rc);
     ck_assert_int_ne(rc, 0);
 }
@@ -57,7 +49,7 @@ START_TEST(test_get_product_name)
 {
     int rc;
     char name[256] = {0};
-    rc = ocl_link_get_product_name(link, name);
+    rc = ocl_link_get_product_name(oclink, name);
     ck_assert_int_eq(rc, 0);
     fprintf(stderr, "Device ProductName is %s\n", name);
 }
@@ -66,7 +58,7 @@ END_TEST
 START_TEST(test_get_device_status)
 {
     int rc;
-    rc = ocl_link_get_device_status(link);
+    rc = ocl_link_get_device_status(oclink);
     ck_assert_int_eq(rc, 0);
     fprintf(stderr, "Device Status is %d\n", rc);
 }
@@ -75,14 +67,12 @@ END_TEST
 static OCL_Temp *temp = NULL;
 static void temp_setup(void)
 {
-    setup();
-    temp = ocl_temp_alloc(link);
+    temp = ocl_temp_alloc(oclink);
 }
 
 static void temp_teardown(void)
 {
     if (temp != NULL) ocl_temp_free(temp);
-    teardown();
 }
 
 START_TEST(test_temp_sensors)
@@ -103,21 +93,45 @@ START_TEST(test_temp_sensors)
 }
 END_TEST
 
+START_TEST(test_temp_limits)
+{
+    int limit;
+    limit = ocl_temp_get_temp_limit(temp);
+    fprintf(stderr, "Temp Limit: %d\n", limit);
+    ck_assert_int_eq(limit, 0);
+}
+END_TEST
+
+static OCL_Fan *fan = NULL;
+
+static void fan_setup(void)
+{
+    fan = ocl_fan_alloc(oclink);
+}
+
+static void fan_teardown(void)
+{
+    if (fan != NULL) ocl_fan_free(fan);
+}
+
+START_TEST(test_fan_ops)
+{
+    int n_fans = ocl_fan_connected_fans(fan);
+    ck_assert_int_eq(n_fans, 5);
+    fprintf(stderr, "Number of connected fans: %d\n", n_fans);
+}
+END_TEST
+
 Suite *create_link_suite(void)
 {
     Suite *suite;
-    TCase *tc_init;
     TCase *tc_core;
     TCase *tc_temp;
+    TCase *tc_fan;
 
     suite = suite_create("Link");
 
-    tc_init = tcase_create("Init");
-    tcase_add_checked_fixture(tc_init, basic_setup, teardown);
-    tcase_add_test(tc_init, test_link_init);
-
     tc_core = tcase_create("Core");
-    tcase_add_checked_fixture(tc_core, setup, teardown);
     tcase_add_test(tc_core, test_link_get_device_id);
     tcase_add_test(tc_core, test_get_fw_version);
     tcase_add_test(tc_core, test_get_product_name);
@@ -127,9 +141,13 @@ Suite *create_link_suite(void)
     tc_temp = tcase_create("Temp");
     tcase_add_checked_fixture(tc_temp, temp_setup, temp_teardown);
     tcase_add_test(tc_temp, test_temp_sensors);
+    tcase_add_test(tc_temp, test_temp_limits);
     suite_add_tcase(suite, tc_temp);
 
-    suite_add_tcase(suite, tc_init);
+    tc_fan = tcase_create("Fan");
+    tcase_add_checked_fixture(tc_fan, fan_setup, fan_teardown);
+    tcase_add_test(tc_fan, test_fan_ops);
+    suite_add_tcase(suite, tc_fan);
 
     return suite;
 }
@@ -139,13 +157,16 @@ int main(void)
     int n_failed;
     Suite *link_suite;
     SRunner *runner;
+    setup();
     link_suite = create_link_suite();
     runner = srunner_create(link_suite);
+    srunner_set_fork_status(runner, CK_NOFORK);
 
     srunner_run_all(runner, CK_ENV);
 
     n_failed = srunner_ntests_failed(runner);
     srunner_free(runner);
+    teardown();
 
     return (n_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
